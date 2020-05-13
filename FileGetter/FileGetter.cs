dc3330cb7db8882ca434340a31d9a6289b5db6e3
oleg.koloskov <oleg.koloskov@ingate.ru>
2020-05-13 20:40:32 +0300

    Добавил раззиповку файлов

--- FileGetter/FileGetter.cs
+++ FileGetter/FileGetter.cs
@@ -22,14 +22,15 @@ namespace FileGetter {
         /// <returns></returns>
         public async Task<FileData> GetFile(string address) {
             var uri = new Uri(address);
-
+            
             for (var i = 0; i < MAX_TRY_COUNT; i++) {
                 var request = (HttpWebRequest) WebRequest.Create(uri);
                 request.Headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
                 request.Headers["Accept-Encoding"] = "gzip, deflate, br";
                 request.Headers["Accept-Language"] = "en-US,en;q=0.9";
                 request.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36";
-
+                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                
                 var result = new FileData();
                 try {
                     using (var response = (HttpWebResponse) (await request.GetResponseAsync())) {
@@ -37,7 +38,7 @@ namespace FileGetter {
                         if (string.IsNullOrEmpty(result.FileName)) {
                             result.FileName = HttpUtility.UrlDecode(uri.Segments.Last());
                         }
-
+                        
                         using (var stream = response.GetResponseStream()) {
                             if (stream != null) {
                                 using (var ms = new MemoryStream()) {
