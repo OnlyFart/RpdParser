45e242419073d786666f6e4f60a980f47bd975d9
oleg.koloskov <oleg.koloskov@ingate.ru>
2020-05-13 12:51:17 +0300

    Доработки получатора имени файла

--- FileGetter/FileNameGetter.cs
+++ FileGetter/FileNameGetter.cs
@@ -1,34 +1,48 @@
 using System;
+using System.Linq;
 using System.Net.Mime;
 using System.Web;
 using NLog;
 
 namespace FileGetter {
-    public class FileNameGetter {
+    public static class FileNameGetter {
         private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
         
         public static string Get(string disposition) {
-            string result = string.Empty;
+            if (string.IsNullOrWhiteSpace(disposition)) {
+                return string.Empty;
+            }
             
-            if (!string.IsNullOrWhiteSpace(disposition)) {
-                try {
-                    result = new ContentDisposition(disposition).FileName;
-                } catch {
-                    var fineNameIndex = disposition.IndexOf("filename", StringComparison.InvariantCultureIgnoreCase);
-                    if (fineNameIndex > -1) {
-                        var quotesIndex = disposition.IndexOf("''", fineNameIndex, StringComparison.InvariantCultureIgnoreCase);
+            var result = string.Empty;
+            
+            try {
+                result = new ContentDisposition(disposition).FileName;
+            } catch {
+                foreach (var item in disposition.Split(";").Select(i => i.Trim())) {
+                    const string FILENAME = "filename";
+                    var fileNameIndex = item.IndexOf(FILENAME, StringComparison.InvariantCultureIgnoreCase);
+                    if (fileNameIndex > -1) {
+                        var quotesIndex = item.IndexOf("''", fileNameIndex, StringComparison.InvariantCultureIgnoreCase);
                         if (quotesIndex > -1) {
-                            result = disposition.Substring(quotesIndex + 2, disposition.Length - quotesIndex - 2);
+                            result = item.Substring(quotesIndex + 2, item.Length - quotesIndex - 2);
                         } else {
-                            quotesIndex = disposition.IndexOf("\"", fineNameIndex, StringComparison.InvariantCultureIgnoreCase);
+                            quotesIndex = item.IndexOf("\"", fileNameIndex, StringComparison.InvariantCultureIgnoreCase);
                             if (quotesIndex > -1) {
-                                result = disposition.Substring(quotesIndex + 1, disposition.Length - quotesIndex - 2);
+                                result = item.Substring(quotesIndex + 1, item.Length - quotesIndex - 2);
                             } else {
-                                _logger.Warn($"Не удалось получить имя файла из {disposition}");
+                                result = item.Substring(fileNameIndex + FILENAME.Length + 1, item.Length - FILENAME.Length - 1);
                             }
                         }
                     }
+
+                    if (!string.IsNullOrWhiteSpace(result)) {
+                        break;
+                    }
                 }
+            }
+
+            if (string.IsNullOrEmpty(result)) {
+                _logger.Warn($"Не удалось получить имя файла из {disposition}");
             }
 
             return HttpUtility.UrlDecode(result);
--- FileGetterTests/FileNameGetterTests.cs
+++ FileGetterTests/FileNameGetterTests.cs
@@ -6,7 +6,11 @@ namespace FileGetterTests {
         [TestCase("", "")]
         [TestCase(null, "")]
         [TestCase("filename*=UTF-8''%D0%A0%D0%9F%D0%A3%D0%94.docx", "РПУД.docx")]
+        [TestCase("filename*=UTF-8''%D0%A0%D0%9F%D0%A3%D0%94.docx;  asfsagas", "РПУД.docx")]
         [TestCase("attachment; filename=\"213-422-1-SM.pdf\"", "213-422-1-SM.pdf")]
+        [TestCase("attachment; filename=\"213-422-1-SM.pdf\"; sagasgsag", "213-422-1-SM.pdf")]
+        [TestCase("attachment; filename=Ñîõðàíåíèå_ïðèðîäíîãî_è_êóëüòóðíîãî_íàñëåäèÿ.doc", "Ñîõðàíåíèå_ïðèðîäíîãî_è_êóëüòóðíîãî_íàñëåäèÿ.doc")]
+        [TestCase("attachment; filename=Ñîõðàíåíèå_ïðèðîäíîãî_è_êóëüòóðíîãî_íàñëåäèÿ.doc; safsagg", "Ñîõðàíåíèå_ïðèðîäíîãî_è_êóëüòóðíîãî_íàñëåäèÿ.doc")]
         public void GetTests(string disposition, string expected) {
             Assert.AreEqual(expected, FileNameGetter.Get(disposition));
         }
